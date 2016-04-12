1774e7dca14f0b95b841830de013a57749387ad3
Jonathan Wexler <jwexman@gmail.com>
2016-04-12 17:33:05 -0400

    final changes

--- client/main.js
+++ client/main.js
@@ -3,36 +3,52 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { Chats } from '../chats.js';
 import './main.html';
 
+// For the Template 'nycda_chat', these are helper methods
 Template.nycda_chat.helpers({
+	// Return true if the post belongs to the current user
+    currentUser(){
+    	return Meteor.userId() == this.user_id;
+    }
+});
+
+// For the body, these are helper methods
+Template.body.helpers({
+	// If the template is looking for 'chats', here we return all of the chats in the DB
     chats: function() {
         return Chats.find({}, { sort: { time: -1}});
     }
 })
 
-Template.nycda_chat.events({
-  'keydown input#text' : function (event) {
-    if (event.which == 13) { // 13 is the enter key event
-      var name = 'Anonymous';
-      var message = $('#text')
+// The following listen and respond user triggered events
+Template.body.events({
+	// If the user presses a keyboard key
+  'keydown input#text' : function (e) {
+  	// We check to see if that key was the 'enter' key
+    if (e.which == 13) { 
+      // Set email to Anon, assuming the user has not yet signed up
+	  let email = "Anon";
+	  // Check to see if there is a logged in user and reassign the email varaible to their email
+      if (Meteor.user() != null) {
+  	      email = Meteor.user().emails[0].address;
+      };
+      // store the user's input text in a variable
+      let message = $('#text');
 
+      // store the user's id in a variable, or set a random number as their id if they are not signed in
+      let id = Meteor.userId() || Math.random();
+
+      // As long as the message isn't empty, store it
       if (message.value != '') {
-      	console.log(Meteor.user().emails[0].address);
         Chats.insert({
-          name: name,
-          message: message.value,
-          time: Date.now(),
+          owner: email,
+          message: message.val(),
+          user_id: id,
+          time: Date.now()
         });
 
+        // Clear the input text box
         $('#text').val('');
-        message.value = '';
       }
     }
   }
 });
-
-// Template.hello.events({
-//   'click button'(event, instance) {
-//     // increment the counter when button is clicked
-//     instance.counter.set(instance.counter.get() + 1);
-//   },
-// });
